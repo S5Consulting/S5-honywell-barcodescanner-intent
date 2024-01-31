cb73889241ac63d9b4213dc99b12d3f701c1f9b6
Charbel Kisso <charbel@s5consulting.se>
2024-01-31 23:05:07 +0100

    Update version and add .idea to .gitignore
    enable profile name as argument

--- src/android/HoneywellScannerPlugin.java
+++ src/android/HoneywellScannerPlugin.java
@@ -17,124 +17,143 @@ import org.apache.cordova.PluginResult;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+public class HoneywellScannerPlugin extends CordovaPlugin {
+  private static final String TAG = "HoneywellScanner";
+  private static final String ACTION_BARCODE_DATA = "com.honeywell.action.BARCODE_DATA";
+  private static final String ACTION_CLAIM_SCANNER = "com.honeywell.aidc.action.ACTION_CLAIM_SCANNER";
+  private static final String ACTION_RELEASE_SCANNER = "com.honeywell.aidc.action.ACTION_RELEASE_SCANNER";
+  private static final String EXTRA_PROFILE = "com.honeywell.aidc.extra.EXTRA_PROFILE";
+  private static final String EXTRA_PROPERTIES = "com.honeywell.aidc.extra.EXTRA_PROPERTIES";
+  private CallbackContext callbackContext;
+  private BroadcastReceiver barcodeDataReceiver;
 
-public class HoneywellScannerPlugin extends CordovaPlugin  {
-    private static final String TAG = "HoneywellScanner";
-    private static final String ACTION_BARCODE_DATA="com.honeywell.action.BARCODE_DATA";
-    private static final String ACTION_CLAIM_SCANNER="com.honeywell.aidc.action.ACTION_CLAIM_SCANNER";
-    private static final String ACTION_RELEASE_SCANNER="com.honeywell.aidc.action.ACTION_RELEASE_SCANNER";
-    private static final String EXTRA_PROFILE="com.honeywell.aidc.extra.EXTRA_PROFILE";
-    private static final String EXTRA_PROPERTIES="com.honeywell.aidc.extra.EXTRA_PROPERTIES";
-    private CallbackContext callbackContext;
-    private BroadcastReceiver barcodeDataReceiver;
+  @Override
+  public void initialize(CordovaInterface cordova, CordovaWebView webView) {
 
-    @Override
-    public void initialize(CordovaInterface cordova, CordovaWebView webView) {
+    super.initialize(cordova, webView);
 
-        super.initialize(cordova, webView);
-
-        barcodeDataReceiver = new BroadcastReceiver(){
-          @Override
-          public void onReceive(Context context,Intent intent){
-            if(ACTION_BARCODE_DATA.equals(intent.getAction())){
-              int version = intent.getIntExtra("version",0);
-              if(version>= 1){
-                setScannedData(intent.getStringExtra("data"));
-              }
-            }
+    barcodeDataReceiver = new BroadcastReceiver() {
+      @Override
+      public void onReceive(Context context, Intent intent) {
+        if (ACTION_BARCODE_DATA.equals(intent.getAction())) {
+          int version = intent.getIntExtra("version", 0);
+          if (version >= 1) {
+            setScannedData(intent.getStringExtra("data"));
           }
-        };
-
-
-    }
-
-    @Override
-    public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext)
-    throws JSONException {
-        if (action.equals("listenForScans")) {
-            this.callbackContext=callbackContext;
-            PluginResult result =new PluginResult(PluginResult.Status.NO_RESULT);
-            result.setKeepCallback(true);
-            this.callbackContext.sendPluginResult(result);
-            if(barcodeDataReceiver!=null){
-              getActivity().getApplicationContext().registerReceiver(barcodeDataReceiver,new IntentFilter(ACTION_BARCODE_DATA));
-              claimScanner();
-              Log.d(TAG,"Registered Successfully");
-            }
         }
-         if (action.equals("nativeReleaseScanner")) {
-           this.callbackContext=callbackContext;
-           PluginResult result =new PluginResult(PluginResult.Status.NO_RESULT);
-           result.setKeepCallback(true);
-           this.callbackContext.sendPluginResult(result);
-           nativeReleaseScanner();
+      }
+    };
+
+  }
+
+  @Override
+  public boolean execute(String action, final JSONArray args, final CallbackContext callbackContext)
+      throws JSONException {
+    if (action.equals("listenForScans")) {
+      this.callbackContext = callbackContext;
+      PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+      result.setKeepCallback(true);
+      this.callbackContext.sendPluginResult(result);
+      if (barcodeDataReceiver != null) {
+        getActivity().getApplicationContext().registerReceiver(barcodeDataReceiver,
+            new IntentFilter(ACTION_BARCODE_DATA));
+
+        // test if args has a profile name and call correct calimScanner method
+        if (args.length() > 0) {
+          String profile = args.getString(0);
+          claimScanner(profile);
+          Log.d(TAG, "Registered Successfully with profile: " + profile);
+        } else {
+          claimScanner();
+          Log.d(TAG, "Registered Successfully");
         }
-        return true;
-    }
-
-
-
-    @Override
-    public void onResume(boolean multitasking) {
-        super.onResume(multitasking);
-        getActivity().getApplicationContext().registerReceiver(barcodeDataReceiver,new IntentFilter(ACTION_BARCODE_DATA));
-        claimScanner();
-    }
-
-    @Override
-    public void onPause(boolean multitasking) {
-        super.onPause(multitasking);
-        try{
-          if(barcodeDataReceiver!=null){
-            getActivity().getApplicationContext().unregisterReceiver(barcodeDataReceiver);
-            releaseScanner();
-          }
-        }catch(IllegalArgumentException e){
-          e.printStackTrace();
-
-        }
-    }
-    private Activity getActivity(){
-      return this.cordova.getActivity();
-    }
-
-    @Override
-    public void onDestroy() {
-        super.onDestroy();
-
-    }
-
-    private void NotifyError(String error) {
-        if (this.callbackContext != null) {
-            PluginResult result = new PluginResult(PluginResult.Status.ERROR, error);
-            result.setKeepCallback(true);
-            this.callbackContext.sendPluginResult(result);
-        }
-    }
-    private void setScannedData(String data){
-      if(this.callbackContext!=null){
-        PluginResult result = new PluginResult(PluginResult.Status.OK, data);
-        result.setKeepCallback(true);
-        this.callbackContext.sendPluginResult(result);
       }
     }
-    private void claimScanner(){
-      Bundle properties = new Bundle();
-      properties.putBoolean("DPR_DATA_INTENT",true);
-      properties.putString("DPR_DATA_INTENT_ACTION",ACTION_BARCODE_DATA);
-      getActivity().getApplicationContext().sendBroadcast(new Intent(ACTION_CLAIM_SCANNER).putExtra(EXTRA_PROFILE,"neptune").putExtra(EXTRA_PROPERTIES,properties));
+    if (action.equals("nativeReleaseScanner")) {
+      this.callbackContext = callbackContext;
+      PluginResult result = new PluginResult(PluginResult.Status.NO_RESULT);
+      result.setKeepCallback(true);
+      this.callbackContext.sendPluginResult(result);
+      nativeReleaseScanner();
     }
-    private void releaseScanner(){
-      getActivity().getApplicationContext().sendBroadcast(new Intent(ACTION_RELEASE_SCANNER));
-    }
-    private void nativeReleaseScanner(){
-      try{
-        if(barcodeDataReceiver!=null){
-          getActivity().getApplicationContext().unregisterReceiver(barcodeDataReceiver);
-          releaseScanner();
-        }
-      }catch(IllegalArgumentException e){
-        e.printStackTrace();
+    return true;
+  }
+
+  @Override
+  public void onResume(boolean multitasking) {
+    super.onResume(multitasking);
+    getActivity().getApplicationContext().registerReceiver(barcodeDataReceiver, new IntentFilter(ACTION_BARCODE_DATA));
+    claimScanner();
+  }
+
+  @Override
+  public void onPause(boolean multitasking) {
+    super.onPause(multitasking);
+    try {
+      if (barcodeDataReceiver != null) {
+        getActivity().getApplicationContext().unregisterReceiver(barcodeDataReceiver);
+        releaseScanner();
       }
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+
     }
+  }
+
+  private Activity getActivity() {
+    return this.cordova.getActivity();
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+
+  }
+
+  private void NotifyError(String error) {
+    if (this.callbackContext != null) {
+      PluginResult result = new PluginResult(PluginResult.Status.ERROR, error);
+      result.setKeepCallback(true);
+      this.callbackContext.sendPluginResult(result);
+    }
+  }
+
+  private void setScannedData(String data) {
+    if (this.callbackContext != null) {
+      PluginResult result = new PluginResult(PluginResult.Status.OK, data);
+      result.setKeepCallback(true);
+      this.callbackContext.sendPluginResult(result);
+    }
+  }
+
+  private void claimScanner() {
+    Bundle properties = new Bundle();
+    properties.putBoolean("DPR_DATA_INTENT", true);
+    properties.putString("DPR_DATA_INTENT_ACTION", ACTION_BARCODE_DATA);
+    getActivity().getApplicationContext().sendBroadcast(
+        new Intent(ACTION_CLAIM_SCANNER).putExtra(EXTRA_PROFILE, "neptune").putExtra(EXTRA_PROPERTIES, properties));
+  }
+
+  private void claimScanner(String profile) {
+    Bundle properties = new Bundle();
+    properties.putBoolean("DPR_DATA_INTENT", true);
+    properties.putString("DPR_DATA_INTENT_ACTION", ACTION_BARCODE_DATA);
+    getActivity().getApplicationContext().sendBroadcast(
+        new Intent(ACTION_CLAIM_SCANNER).putExtra(EXTRA_PROFILE, profile).putExtra(EXTRA_PROPERTIES, properties));
+  }
+
+  private void releaseScanner() {
+    getActivity().getApplicationContext().sendBroadcast(new Intent(ACTION_RELEASE_SCANNER));
+  }
+
+  private void nativeReleaseScanner() {
+    try {
+      if (barcodeDataReceiver != null) {
+        getActivity().getApplicationContext().unregisterReceiver(barcodeDataReceiver);
+        releaseScanner();
+      }
+    } catch (IllegalArgumentException e) {
+      e.printStackTrace();
+    }
+  }
 }
--- www/honeywell.js
+++ www/honeywell.js
@@ -1,12 +1,30 @@
 var execute = require("cordova/exec");
 
 var honeywell = {
-
-    listenForScans:function(success,failure){
-      return execute(success,failure,'HoneywellScannerPlugin', 'listenForScans', []);
-    },
-    nativeReleaseScanner:function(success,failure){
-        return execute(success,failure,'HoneywellScannerPlugin', 'nativeReleaseScanner', []);
-    }
+  /**
+   * Listens for scan events from the Honeywell scanner
+   * @param {*} success
+   * @param {*} failure
+   * @param {Array} args
+   * @returns
+   */
+  listenForScans: function (success, failure, args) {
+    return execute(
+      success,
+      failure,
+      "HoneywellScannerPlugin",
+      "listenForScans",
+      args
+    );
+  },
+  nativeReleaseScanner: function (success, failure) {
+    return execute(
+      success,
+      failure,
+      "HoneywellScannerPlugin",
+      "nativeReleaseScanner",
+      []
+    );
+  },
 };
 module.exports = honeywell;
